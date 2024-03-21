2decc1d074af261a07531d530146432e1c71085d
muhammadabdullahi190A-u-usman <muhammadabdullahi190@gmail.com>
2024-03-21 09:00:59 +0100

    python assignment

---
+++ user_input.py
@@ -0,0 +1,8 @@
+#Week one python assignment
+#Muhammad Abdullahi Usman
+
+
+name = input("Enter your Name: ")
+age = input("Enter your age: ")
+location = input("Enter your Location: ")
+print(f"{name} you are Welcome to plp, You are {age} years old and live in {location}")
